73e7ea2c20813960589ac0175a24371a4440c9da
최은철 <wndqhrgkdlel@gmail.com>
2024-01-02 14:49:42 +0900

    일단 되돌리기

--- src/right.js
+++ src/right.js
@@ -1,21 +1,17 @@
 import React, { useState, useEffect } from "react";
-import './App.css'; 
 
 const Right = ({ membersData, selectedName }) => {
   const [searchName, setSearchName] = useState("");
-  const [memberInfo, setMemberInfo] = useState(null);
+  const [filteredMembers, setFilteredMembers] = useState([]);
 
   useEffect(() => {
     const targetName = searchName || selectedName;
 
-    const foundMember =
-      membersData &&
-      Object.values(membersData).find((member) => member.name === targetName);
-
-    if (foundMember) {
-      setMemberInfo(foundMember);
+    if (targetName) {
+      const foundMember = membersData && membersData[targetName];
+      setFilteredMembers(foundMember ? [foundMember] : []);
     } else {
-      setMemberInfo(null);
+      setFilteredMembers(Object.values(membersData));
     }
   }, [membersData, searchName, selectedName]);
 
@@ -26,12 +22,14 @@ const Right = ({ membersData, selectedName }) => {
   return (
     <div className="input-form">
       <div className="member-info">
-        {memberInfo ? (
-          <>
-            <h3>{memberInfo.name} 소개</h3>
-            <p>자기 소개: {memberInfo.mainParagraph}</p>
-            <p>별명: {memberInfo.subParagraph}</p>
-          </>
+        {filteredMembers.length ? (
+          filteredMembers.map((member) => (
+            <div key={member.name}>
+              <h3>{member.name} 소개</h3>
+              <p>자기 소개: {member.mainParagraph}</p>
+              <p>별명: {member.subParagraph}</p>
+            </div>
+          ))
         ) : (
           <p>멤버를 찾을 수 없습니다.</p>
         )}
